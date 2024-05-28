8f5f5355d606aca5d767755bd6a04ba1d3990542
Cliff <developerlyk@gmail.com>
2024-05-28 20:57:10 +0900

    xml 기반 appconfig

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,6 +1,6 @@
 package hello.core.order;
 
-import hello.core.dicsount.DiscountPolicy;
+import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 
--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -1,8 +1,8 @@
 package hello.core.beanfind;
 
-import hello.core.dicsount.DiscountPolicy;
-import hello.core.dicsount.FixDiscountPolicy;
-import hello.core.dicsount.RateDiscountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
