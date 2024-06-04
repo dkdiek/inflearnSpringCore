918e52c96dce622101dfe0b18ab7906e7554050a
Cliff <developerlyk@gmail.com>
2024-06-04 21:26:55 +0900

    bean scope

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,41 @@
+package hello.core.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+/**
+ * hello.core.common
+ * MyLogger
+ *
+ * @author : K
+ */
+@Component
+@Scope("request")
+public class MyLogger {
+  
+  private String uuid;
+  private String requestURL;
+  
+  public void setRequestURL(String requestURL) {
+    this.requestURL = requestURL;
+  }
+  
+  public void log(String message) {
+    System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
+  }
+  
+  @PostConstruct
+  public void init() {
+    uuid = UUID.randomUUID().toString();
+    System.out.println("[" + uuid + "]" + "[" + requestURL + "] request scope create: " + this );
+  }
+  
+  @PreDestroy
+  private void close() {
+    System.out.println("[" + uuid + "]" + "[" + requestURL + "] request scope create: " + this );
+  }
+}
